de32b28caace8998f82b494b1444d7a911fddefa
Lukasz A. Bartnik <l.bartnik@gmail.com>
2014-12-09 01:25:28 +0100

    adding more code but it already needs refactoring (see repo.r)

---
+++ somepackage/R/collection.r
@@ -0,0 +1,6 @@
+# 0. move collection-related code here
+# 1. start with a collection (forget about reposiories)
+# 2. enable adding and search objects in a collection (tags!) + TDD with testthat
+# 3. implement run_pipe method for a selection of objects/whole collection
+# 4. implement storing results back to a collection
+# 5. add repositories
--- somepackage/R/repo.r
+++ somepackage/R/repo.r
@@ -12,15 +12,16 @@ create_repo <- function (path, create = F) {
     dir.create(path, showWarnings = F, recursive = T)
   }
   
-  structure(list(path = path), class = 'repo')
+  repo <- structure(list(path = path), class = 'repo')
+  create_collection(repo, 'default')
+  
+  repo
 }
-
 
 #' @export
 is_repo <- function (x) {
   inherits(x, 'repo')
 }
-
 
 #' @export
 is_repo_path <- function (path, quiet = F) {
@@ -31,11 +32,10 @@ is_repo_path <- function (path, quiet = F) {
 
 
 #' @export
-#' @importFrom digest digest
 create_collection <- function (repo, name) {
   stopifnot(is_repo(repo))
-  h <- digest(name, 'md5')
-  p <- file.path(repo$path, h)
+  cid <- compute_id(name)
+  p <- file.path(repo$path, cid)
   if (file.exists(p)) warning('collection already exists')
   else {
     dir.create(p)
@@ -45,10 +45,12 @@ create_collection <- function (repo, name) {
 }
 
 #' @export
-as_collection <- function (repo, col_name = 'default') {
+as_collection <- function (repo, col_name) {
   stopifnot(is_repo(repo))
   if (is_collection(repo)) repo$collection <- col_name
   else {
+    if (!has_collection(repo, col_name))
+      stop('collection ', col_name, ' does not exist', call. = F)
     class(repo) <- c('collection', class(repo))
     repo$collection <- name  
   }
@@ -56,51 +58,115 @@ as_collection <- function (repo, col_name = 'default') {
 }
 
 #' @export
-is_collection <- function (x) {
-  inherits(x, 'collection')
+in_collection <- as_collection
+
+#' @export
+in_default_collection <- function (repo) in_collection(repo, 'default')
+
+#' @export
+is_collection <- function (x) inherits(x, 'collection')
+
+
+
+#' @importFrom digest digest
+compute_id <- function (x) digest(x, 'md5')
+
+#' @importFrom assertthat has_attr
+
+with_id <- function (object) {
+  if (has_attr(object 'id')) return(object)
+  attr(object, 'id') <- compute_id(object)
+  object
+}
+
+#' @importFrom assertthat has_attr
+id <- function (x) {
+  stopifnot(has_attr(x, 'id'))
+  attr(x, 'id')
+}
+
+id_name <- function (x) {
+  paste0(id(x), '.rds')
 }
 
 
 #' @export
-add_object <- function (...) UseMethod('add_object')
+add_object <- function (..., .dots) UseMethod('add_object')
 
-#' @export
-add_object.repo <- function (repo, object, col_name = 'default') {
+#' @importFrom lazyeval all_dots
+add_object.repo <- function (repo, object, ..., .dots) {
   if (!has_collection(repo, col_name))
     cl <- create_collection(repo, col_name)
   else
     cl <- as_collection(repo, col_name)
-  add_object.collection(cl, object)
+  
+  add_object_.collection(cl, object, ..., .dots = dots)
+}
+
+#' @importFrom digest digest
+add_object.collection <- function (collection, object, ..., .dots) {
+  object <- with_id(object)
+  if (has_object(collection, object)) {
+    warning('object already exists', call. = F)
+    return(id(object))
+  }
+  
+  object <- with_tags_(object, ..., .dots = dots)
+  cid <- compute_id(collection$collection)
+  saveRDS(object, file.path(collection$path, cid, id_name(object)))
+}
+
+
+#' @export
+#' @importFrom lazyeval lazy_dots
+with_tags <- function (object, ...) {
+  with_tags_(object, .dots = lazy_dots(...))
 }
 
 #' @export
-#' @importFrom digest digest
-add_object.collection <- function (collection, object) {
-  id <- digest(object, 'md5')
-  if (has_object(collection, object)) {
-    warning('object already exists', call. = F)
-    return(id)
-  }
-  cp <- digest(collection$collection, 'md5')
-  on <- paste0(id, '.rds')
-  saveRDS(object, file.path(collection$path, cp, on))
+#' @importFrom lazyeval all_dots lazy_eval
+#' @importFrom assertthat has_name
+with_tags_ <- function (object, ..., .dots) {
+  dots <- all_dots(.dots, ..., all_named = TRUE) # TODO maybe tags with no names?
+  tags <- lazy_eval(dots, list(. = object))
+  if (has_name(tags, 'id')) stop('cannot overwrite object id', call. = F)
+  attributes(object) <- c(attributes(object), tags)
+  object
 }
 
 
 #' @export
 has_object <- function (...) UseMethod('has_object')
 
-#' @export
+#'
 has_object.repo <- function (repo, object, col_name = 'default') {
   if (!has_collection(repo, col_name)) return(F)
   has_object(as_collection(repo, col_name), object)
 }
 
-#' @export
 #' @importFrom digest digest
 has_object.collection <- function (collection, object) {
+  object <- with_id(object)
   cp <- digest(collection$collection, 'md5')
-  on <- paste0(digest(object, 'md5'), '.rds')
-  file.exists(file.path(collection$path, cp, on))
+  file.exists(file.path(collection$path, cp, id_name(object)))
+}
+
+
+#' @export
+select <- function (.where, ...) {
+  select_(.where, .dots = lazy_dots(...))
+}
+
+#' @export
+select_ <- function (.where, ..., .dots) UseMethod('select_')
+
+#' @importFrom lazyeval all_dots lazy_eval
+select_.repo <- function(repo, ..., .dots) {
+  dots <- all_dots(.dots, ..., named = T)
+  dots <- lazy_eval(dots)
+}
+
+select_.collection <- function(collection, ..., .dots) {
+  
 }
 
