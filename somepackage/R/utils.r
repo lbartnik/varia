97fff81f120cd1dcad8f9bd59d4087f002fc3d9b
Lukasz A. Bartnik <l.bartnik@gmail.com>
2015-02-02 22:07:39 +0100

    removing old sources

--- somepackage/R/utils.r
+++
@@ -1,9 +0,0 @@
-#' @importFrom assertthat see_if
-see <- function (..., quiet = F) {
-  r <- see_if(..., env = parent.frame())
-  if (!r && !quiet) warning(attr(r, 'msg'), call. = F)
-  r
-}
-
-`%nin%` <- function (x, y) (!(x %in% y))
-
